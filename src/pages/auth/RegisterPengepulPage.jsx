48085d15afab894e27c92e73a1946370203a9031
Joshtri <stuffofyos1516@gmail.com>
2024-12-04 21:39:53 +0800

    adding new page

---
+++ src/pages/auth/RegisterPengepulPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../Layout'
+import RegisterPengepul from '../../components/auth/RegisterPengepul'
+
+export default function RegisterPengepulPage() {
+  return (
+    <Layout>
+        <RegisterPengepul/>
+    </Layout>
+  )
+}
