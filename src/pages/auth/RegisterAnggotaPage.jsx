6dddcc1267a6dc3ac2353e978151817684ff8e66
Joshtri <stuffofyos1516@gmail.com>
2024-12-06 15:02:55 +0800

    update : rename file and function

---
+++ src/pages/auth/RegisterAnggotaPage.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import Layout from '../Layout'
+import RegisterAnggota from '../../components/auth/RegisterAnggota'
+
+function RegisterPage() {
+  return (
+    <Layout>
+        <RegisterAnggota/>
+    </Layout>
+  )
+}
+
+export default RegisterPage
