03ecd3dec60697cc735b3ce19cce6f1918cc3056
Joshtri <stuffofyos1516@gmail.com>
2024-12-06 15:02:02 +0800

    update : rename file and  function

---
+++ src/pages/auth/LoginAnggotaPage.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import Layout from '../Layout'
+import LoginAnggota from '../../components/auth/LoginAnggota'
+
+function LoginPage() {
+  return (
+    <Layout>
+        {/* <MainContent/> */}
+        <LoginAnggota/>
+    </Layout>
+  )
+}
+
+export default LoginPage
