1cf36c73d327f379f3b32d03524da4060e8ef0cc
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:23:10 +0800

    adding scroll to zero

--- src/pages/User/DashboardPage.jsx
+++ src/pages/User/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Dashboard from '../../components/Dashboard'
 import LayoutUser from '../LayoutUser'
 
@@ -9,6 +9,11 @@ function DashboardPage() {
     const toggleSidebar = () => {
       setIsSidebarOpen(!isSidebarOpen);
     };
+
+    useEffect(() => {
+      // Scroll to the top of the page when the component mounts
+      window.scrollTo(0, 0);
+    }, []); // The empty dependency array ensures this runs only once when the component mounts
   return (
     <LayoutUser isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}>
         <Dashboard/>
