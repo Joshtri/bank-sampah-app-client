84bb0b64b65fe884de531a4e22d50ae13753f0d3
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:25:26 +0800

    adding scroll to zero

--- src/pages/auth/RegisterAnggotaPage.jsx
+++ src/pages/auth/RegisterAnggotaPage.jsx
@@ -1,8 +1,13 @@
 // import React from 'react'
 import Layout from '../Layout'
 import RegisterAnggota from '../../components/auth/RegisterAnggota'
+import { useEffect } from 'react';
 
 function RegisterPage() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []); // The empty dependency array ensures this runs only once when the component mounts
   return (
     <Layout>
         <RegisterAnggota/>
