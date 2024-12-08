4cf1e5d74f59877e57faf50f9872a79ba9c7f8c4
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:27:38 +0800

    delete unused

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import {  useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import errorImg from '../assets/3737258.png';
 
