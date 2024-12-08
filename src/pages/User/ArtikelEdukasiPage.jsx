3953cd1526d5d1bf610576c6125752cefa9a0538
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:22:29 +0800

    adding scroll to zero

--- src/pages/User/ArtikelEdukasiPage.jsx
+++ src/pages/User/ArtikelEdukasiPage.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ArtikelEdukasi from '../../components/User/ArtikelEdukasi'
+import LayoutUser from '../LayoutUser'
 
 export default function ArtikelEdukasiPage() {
+
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []); // The empty dependency array ensures this runs only once when the component mounts
   return (
-    <div>
+    <LayoutUser>
         <ArtikelEdukasi/>
-    </div>
+    </LayoutUser>
   )
 }
