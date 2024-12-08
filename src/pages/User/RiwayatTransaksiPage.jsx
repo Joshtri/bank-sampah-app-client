d0f19020f74d8733948f12a414496efd8734ffbd
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:24:02 +0800

    adding scroll to zero

--- src/pages/User/RiwayatTransaksiPage.jsx
+++ src/pages/User/RiwayatTransaksiPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RiwayatTransaksi from '../../components/User/RiwayatTransaksi/RiwayatTransaksi'
 import LayoutUser from '../LayoutUser';
 
@@ -9,6 +9,11 @@ export default function RiwayatTransaksiPage() {
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []); // The empty dependency array ensures this runs only once when the component mounts
   return (
     <LayoutUser isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}>
         <RiwayatTransaksi/>
