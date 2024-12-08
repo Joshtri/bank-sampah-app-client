e91eac67c281596ab62a9d21161f956d89e42145
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:25:46 +0800

    adding scroll to zero

--- src/pages/auth/RegisterPengepulPage.jsx
+++ src/pages/auth/RegisterPengepulPage.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../Layout'
 import RegisterPengepul from '../../components/auth/RegisterPengepul'
 
 export default function RegisterPengepulPage() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []); // The empty dependency array ensures this runs only once when the component mounts
   return (
     <Layout>
         <RegisterPengepul/>
