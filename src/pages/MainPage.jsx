5492a86fa3604e4d39c97ea0a17d3f27d23bd95e
Joshtri <stuffofyos1516@gmail.com>
2024-12-08 16:21:35 +0800

    adding mainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,14 @@
 // import React from 'react'
+import { useEffect } from 'react';
 import JoinUsWithMap from '../components/JoinUsWithMap'
 import MainContent from '../components/MainContent'
 import Layout from './Layout'
 
 function MainPage() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []); // The empty dependency array ensures this runs only once when the component mounts
   return (
     <Layout>
         <MainContent/>
